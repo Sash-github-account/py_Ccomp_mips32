b53cbc2193cab80511d231b40e01451241665291
Sashwath Nalin Kanth <55643895+Sash-github-account@users.noreply.github.com>
2024-07-27 01:21:36 -0400

    IR2AssmblyConvrtr version 2:
        -> Added pseudo-method for multiplication operation

--- IR2Assembly.py
+++ IR2Assembly.py
@@ -7,8 +7,7 @@ Created on Thu Jul 25 19:21:41 2024
 Summary: Contains the class to convert intermediate dictionary created by ParserNOperationSeqr object into a sequencee of assembly instructions 
 
 Version 1:
-    -> Added initial code for IR2AssmblyConvrtr class
-    -> method to process parserNopernseqr's parse_event_dict added: initial version W.I.P
+    -> Added pseudo-method for multiplication operation
 
 """
 import re
@@ -23,7 +22,7 @@ class IR2AssmblyConvrtr:
     
     
     def __init__(self, base_size):
-        self.data_sec_BA = base_size*4
+        self.data_sec_BA = (base_size+2)*4
         self.used_addr_cntr = 0
         self.assembly_instr_lst = []
         self.unused_reg_lst = [
@@ -60,7 +59,7 @@ class IR2AssmblyConvrtr:
                             "r1"
                             ]
         self.used_regs_stk = []
-        self.var_addr_map = {}
+        self.var_addr_map = {self.data_sec_BA: str(self.data_sec_BA), (self.data_sec_BA+4):'0',  (self.data_sec_BA+8):'1'}
         self.data_base_addr_reg = ''
 
 
@@ -72,9 +71,11 @@ class IR2AssmblyConvrtr:
         return None
 
 
+
     def init_system_setup(self):
         self.data_base_addr_reg = self.get_unused_reg()
-        
+        pass
+
 
 
     def get_unused_reg(self):
@@ -82,10 +83,13 @@ class IR2AssmblyConvrtr:
         self.used_regs_stk.append(reg)
         return reg
 
+
+
     def construct_st_instr(self, rt, target_var):
         rs = self.data_base_addr_reg
         addr_offst = self.var_addr_map[target_var]
         st_instr_str = 'sw' + ' $' + rt + ' ' + str(addr_offst) + '(' + " $" + rs + ')'
+  
         return rt, st_instr_str        
 
 
@@ -106,20 +110,31 @@ class IR2AssmblyConvrtr:
             opr_str_tkn = 'AND'
         elif(opr == '|'):
             opr_str_tkn = 'OR'
-        elif(opr == '<<'):
+        elif(opr == '<'):
             opr_str_tkn = 'slt'
         else:
             print('ASSEM ERROR')
         return opr_str_tkn
+
+
 
     def construct_R_type_instruction_seq(self, rs, rt, opr, shamt):
         rd = self.get_unused_reg()
         opr_str = self.get_operation_assem_token_for_R_instr(opr)
         R_instr_str = opr_str + ' $' + rd + ' $' + rs + ' $' + rt
         return rd, R_instr_str
+
+
+
+    def return_register(self, reglist):
+        for item in reglist:
+            reg = self.used_regs_stk.pop()
+            self.unused_reg_lst.append(reg)
+        pass
         
-        
-    def construct_arith_operation_instr_seq(self, target_var, opnd_var1, opnd_var2, opr, shamt = 0):
+    
+    
+    def construct_basic_32bmips_operation_instr_seq(self, target_var, opnd_var1, opnd_var2, opr, shamt = 0):
         rs, lw_inst1 = self.construct_ld_instr(opnd_var1)
         rt, lw_inst2 = self.construct_ld_instr(opnd_var2)
         rd, arith_inst_str = self.construct_R_type_instruction_seq(rs, rt, opr, shamt)
@@ -128,6 +143,22 @@ class IR2AssmblyConvrtr:
         self.assembly_instr_lst.append(lw_inst2)
         self.assembly_instr_lst.append(arith_inst_str)
         self.assembly_instr_lst.append(st_instr_str)
+        self.return_register([rs, rt, rd])
+        pass
+        
+        
+    def process_multiplication_operation(self, target_var, opnd_var1, opnd_var2, opr):
+        # load const 0 in a reg0 #
+        # load opnd_var1 val into another reg1 #
+        # load opnd_var2 into 3rd reg2 #
+        # load const 1 in reg4 #
+        # load const 0 in reg5 #
+        # add reg0 + reg1 into reg0 #
+        # sub reg2 - reg4 into reg2
+        # set reg6 if less than: reg5 < reg2
+        # branch on equal: reg6 == reg4 : -(4*3=12) #
+        pass
+        
         
         
     def process_event_seq(self, event_seq_dict):
@@ -138,6 +169,14 @@ class IR2AssmblyConvrtr:
                 self.used_addr_cntr += 4
             elif(re.search(PnOseqr.re_get_var_name, event_seq_dict[i][0])):
                 opr_fnd = self.find_operator(event_seq_dict[i][1])
-                if(opr_fnd):
+                isany_instr_op = opr_fnd == '+' or opr_fnd == '-' or opr_fnd == '&' or opr_fnd == '|' or opr_fnd == '<'
+                if(isany_instr_op):
                     opnd_lst = re.findall("(" + PnOseqr.re_get_var_name + ')', event_seq_dict[i][1])
-                    
+                    self.construct_basic_32bmips_operation_instr_seq( event_seq_dict[i][0], opnd_lst[0], opnd_lst[1], opr_fnd, 0)
+                elif(opr_fnd == '>'):
+                    opr_fnd = '<'
+                    opnd_lst = re.findall("(" + PnOseqr.re_get_var_name + ')', event_seq_dict[i][1])
+                    self.construct_basic_32bmips_operation_instr_seq( event_seq_dict[i][0], opnd_lst[1], opnd_lst[0], opr_fnd, 0)
+                elif(opr_fnd == '*'):
+                    opnd_lst = re.findall("(" + PnOseqr.re_get_var_name + ')', event_seq_dict[i][1])
+                    self.process_multiplication_operation(event_seq_dict[i][0], opnd_lst[0], opnd_lst[1], opr_fnd)
